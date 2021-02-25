a1c51d73403936199436ddd57d2f45845a212a42
Tamim-Ahmad <73168399+Tamim-Ahmad@users.noreply.github.com>
2021-02-26 02:02:46 +0430

    Initialize refactoring/better_ordering

---
+++ README.md
@@ -0,0 +1,23 @@
+# django-project-template-py3
+A project template for Django 1.11 in Python 3
+
+## Get the code
+Create a new repository for your django project and clone your repository into
+your computer.
+
+Add the django-project-template-py3 github repo as a remote repository:
+* `git remote add template
+  git@github.com:magnet-cl/django-project-template-py3.git`
+
+Pull the code from the project template:
+* `git pull template master`
+
+Push to your own repo
+* `git push origin master`
+
+Now you have your own django project in your repository.
+
+## Start new app
+Use the custom app template to create your apps:
+
+    python manage.py startapp --template=project/app_template answers
---
+++ quickstart.sh
@@ -0,0 +1,185 @@
+#!/bin/bash
+
+function print_green(){
+    echo -e "\033[32m$1\033[39m"
+}
+
+function replace(){
+    echo $1 $2
+    if [ "$OS" == "Darwin" ] ; then
+        echo $i|sed -i '' $1 $2
+    else
+        echo $i|sed -i $1 $2
+    fi
+}
+
+INSTALL_SYSTEM_DEPENDENCIES=true
+INSTALL_PIP=true
+INSTALL_BOWER=true
+INSTALL_NPM=true
+TRANSLATE=true
+BUILD_JAVASCRIPT=true
+
+#!/bin/sh
+
+case "$(uname -s)" in
+
+   Darwin)
+     OS='Darwin'
+     ;;
+
+   Linux)
+     OS='Linux'
+     ;;
+
+   *)
+    echo "OS not supported"
+    exit
+    ;;
+esac
+
+
+while getopts “napbj” OPTION
+do
+    case $OPTION in
+        a)
+             print_green "only install aptitude"
+             INSTALL_SYSTEM_DEPENDENCIES=true
+             INSTALL_PIP=false
+             INSTALL_BOWER=false
+             INSTALL_NPM=false
+             TRANSLATE=false
+             BUILD_JAVASCRIPT=false
+             ;;
+        p)
+             print_green "only pip install"
+             INSTALL_SYSTEM_DEPENDENCIES=false
+             INSTALL_PIP=true
+             INSTALL_BOWER=false
+             INSTALL_NPM=false
+             TRANSLATE=false
+             BUILD_JAVASCRIPT=false
+             ;;
+        b)
+             print_green "only bower install"
+             INSTALL_SYSTEM_DEPENDENCIES=false
+             INSTALL_PIP=false
+             INSTALL_BOWER=true
+             INSTALL_NPM=false
+             TRANSLATE=false
+             BUILD_JAVASCRIPT=false
+             ;;
+        n)
+             print_green "only node install"
+             INSTALL_SYSTEM_DEPENDENCIES=false
+             INSTALL_PIP=false
+             INSTALL_BOWER=false
+             INSTALL_NPM=true
+             TRANSLATE=false
+             BUILD_JAVASCRIPT=false
+             ;;
+        j)
+             print_green "only npm run build"
+             INSTALL_SYSTEM_DEPENDENCIES=false
+             INSTALL_PIP=false
+             INSTALL_BOWER=false
+             INSTALL_NPM=false
+             TRANSLATE=false
+             BUILD_JAVASCRIPT=true
+             ;;
+        ?)
+             print_green "fail"
+             exit
+             ;;
+     esac
+done
+
+if  $INSTALL_SYSTEM_DEPENDENCIES ; then
+    if [ "$OS" == "Darwin" ] ; then
+        print_green "Installing python 3"
+        brew install python3
+
+        print_green "Installing virtualenv"
+        pip install virtualenv
+    else
+        print_green "Installing aptitude dependencies"
+
+        # Install base packages
+        sudo apt-get -y install python-pip python-virtualenv python3-dev build-essential
+
+        print_green "Installing image libraries"
+        # Install image libs
+        sudo apt-get -y install libjpeg-dev zlib1g-dev zlib1g-dev
+
+        print_green "Installing translation libraries"
+        sudo apt-get -y install gettext
+    fi
+
+    print_green "Are you going to use postgre for your database? [Y/n]"
+    read INSTALL_POSTGRE
+
+    if [[ "$INSTALL_POSTGRE" == "Y" ||  "$INSTALL_POSTGRE" == "y" ||  "$INSTALL_POSTGRE" == "" ]]
+    then
+        INSTALL_POSTGRE=true
+        if [ "$OS" == "Darwin" ] ; then
+            if brew ls --versions postgresql > /dev/null ; then
+                echo 'postgresql is already installed'
+            else
+                brew install postgresql
+            fi
+        else
+            ./install/postgres.sh
+        fi
+    fi
+
+    if [ ! -d ".env" ]; then
+        print_green "set a new virtual environment"
+        virtualenv -p python3 .env
+    fi
+fi
+if  $INSTALL_PIP ; then
+    print_green "Installing pip requirements on requirement.txt"
+
+    # activate the environment
+    source .env/bin/activate
+
+    # install setuptools
+    pip install --upgrade setuptools
+
+    # upgrade pip
+    pip install --upgrade pip
+
+    # install pip requirements in the virtual environment
+    .env/bin/pip install --requirement requirements.txt
+fi
+
+# create the local_settings file if it does not exist
+if [ ! -f ./project/settings/local_settings.py ] ; then
+    cp project/local_settings.py.default project/local_settings.py
+
+    if [ INSTALL_POSTGRE ] ; then
+        replace "s/database-name/${PWD##*/}/g" project/local_settings.py
+
+        print_green "remember to configure in project/local_setings.py your database"
+    else
+        replace "s/postgresql_psycopg2/sqlite3/g" project/local_settings.py
+
+        replace "s/database-name/\/tmp/${PWD##*/}.sql/g" project/local_settings.py
+    fi
+fi
+
+# Change the project/settings.py file if it contains the CHANGE ME string
+if grep -q "CHANGE ME" "project/settings.py"; then
+    print_green "Generate secret key"
+    # change the SECRET_KEY value on project settings
+    python manage.py generatesecretkey
+fi
+
+
+if  $INSTALL_NPM ; then
+    # package.json modification
+    replace "s/NAME/${PWD##*/}/g" package.json
+    replace "s/HOMEPAGE/https:\/\/bitbucket.org\/magnet-cl\/${PWD##*/}/g" package.json
+
+    npm install
+fi
